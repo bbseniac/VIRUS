43a552fb19171afee8c3eedbead3d7af8caeb4b1
bbseniac <73187586+bbseniac@users.noreply.github.com>
2020-10-23 19:56:07 +0530

    Update particles.js

--- particles.js
+++ particles.js
@@ -20,7 +20,7 @@ var pJS = function(tag_id, params){
     },
     particles: {
       number: {
-        value: 450
+        value: 10
         density: {
           enable: true,
           value_area: 800
